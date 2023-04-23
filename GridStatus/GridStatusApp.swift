9b27943697b7ee9c2ca7dc1bcb86258a2f59dc63
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2023-04-23 11:20:29 -0400

    Initial Commit

---
+++ GridStatus/GridStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridStatusApp.swift
+//  GridStatus
+//
+//  Created by Nicolas Le Gorrec on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GridStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GridStatusUITests/GridStatusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GridStatusUITestsLaunchTests.swift
+//  GridStatusUITests
+//
+//  Created by Nicolas Le Gorrec on 4/23/23.
+//
+
+import XCTest
+
+final class GridStatusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
