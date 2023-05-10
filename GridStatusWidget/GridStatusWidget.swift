a97c256840575a62c3c562081426810f0120a5a2
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2023-05-10 13:33:50 -0400

    widget mock

--- GridStatusWidget/GridStatusWidget.swift
+++ GridStatusWidget/GridStatusWidget.swift
@@ -7,6 +7,7 @@
 
 import WidgetKit
 import SwiftUI
+import Core
 import Intents
 import GridStatusCommonUI
 
@@ -14,18 +15,27 @@ struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         return SimpleEntry(
             date: Date(),
-            configuration: ConfigurationIntent()
+            configuration: ConfigurationIntent(), data: []
         )
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         print("snaps")
-        completion(SimpleEntry(date: Date(), configuration: configuration))
+        completion(SimpleEntry(date: Date(), configuration: configuration, data: []))
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        print("timelines")
-        let timeline = Timeline(entries: [SimpleEntry(date: Date(), configuration: ConfigurationIntent())], policy: .atEnd)
+        let data = StandardFiveMinuteResponse.example.data.map { StackedAreaChartItem($0) }
+        print("\(data.count)")
+        print("new")
+
+        let timeline = Timeline(entries: [
+            SimpleEntry(date: Date(),
+                        configuration: ConfigurationIntent(),
+                        data: data
+                       )
+        ],
+        policy: .atEnd)
         completion(timeline)
     }
 }
@@ -33,6 +43,7 @@ struct Provider: IntentTimelineProvider {
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
+    let data: [StackedAreaChartItem]
 }
 
 struct GridStatusWidgetEntryView : View {
@@ -41,7 +52,7 @@ struct GridStatusWidgetEntryView : View {
     var body: some View {
         StackedAreaChartCard(
             config: .init(isoId: "caiso", dataType: "Fuel Mix"),
-            datas: [],
+            datas: entry.data,
             timeZone: .current
         )
     }
@@ -62,7 +73,7 @@ struct GridStatusWidget: Widget {
 
 struct GridStatusWidget_Previews: PreviewProvider {
     static var previews: some View {
-        GridStatusWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        GridStatusWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), data: []))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
